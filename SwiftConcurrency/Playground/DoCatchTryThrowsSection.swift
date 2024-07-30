c0316b751515e5309145581dde8d4f619e6a9ef5
Gray <grayzprogrammer@gmail.com>
2024-07-30 15:54:11 -0500

    Error Handling review completed

--- SwiftConcurrency/Playground/DoCatchTryThrowsSection.swift
+++ SwiftConcurrency/Playground/DoCatchTryThrowsSection.swift
@@ -7,9 +7,25 @@
 
 import SwiftUI
 
+enum PlaygroundError: Error {
+    case badResponse
+    
+    var errorDescription: String {
+        switch self {
+        case .badResponse:
+            "Bad Response"
+        }
+    }
+}
+
 class DoCatchTryThrowsDataManager {
-    func getContent() -> String {
-       return "Updated Content"
+    // isActive is a constant and always be FALSE.
+    // getContent() will alwasy return nil value.
+    let isActive = false
+    
+    func getContent() throws -> String {
+        guard isActive else { throw PlaygroundError.badResponse }
+        return "Updated Content"
     }
 }
 
@@ -19,7 +35,13 @@ class DoCatchTryThrowsViewModel: ObservableObject {
     let manager = DoCatchTryThrowsDataManager()
     
     func fetch() {
-        content = manager.getContent()
+        do {
+            self.content = try manager.getContent()
+        } catch let error as PlaygroundError {
+            self.content = error.errorDescription
+        } catch {
+            self.content = error.localizedDescription
+        }
     }
 }
 
@@ -30,7 +52,7 @@ struct DoCatchTryThrowsSection: View {
         Text(viewModel.content)
             .font(.title)
             .padding()
-            .frame(width: 300, height: 200)
+            .frame(width: 300, height: 500)
             .background(.red.opacity(0.2))
             .clipShape(RoundedRectangle(cornerRadius: 16))
             .onTapGesture {
